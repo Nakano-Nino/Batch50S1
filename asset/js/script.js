8a4d6de6cb7e03b3f96c0a8b79b06b7052532d52
Nakano-Nino <yudhaprasty17@gmail.com>
2023-09-27 13:11:34 +0700

    Day 3

---
+++ asset/js/script.js
@@ -0,0 +1,34 @@
+function getData(){
+    const name = document.getElementById("name").value
+    const email = document.getElementById("email").value
+    const phone = document.getElementById("phone").value
+    const subject = document.getElementById("subject").value
+    const message = document.getElementById("message").value
+
+    if (name == ""){
+        return alert("nama tidak boleh kosong")
+    } else if (email == ""){
+        return alert("email tidak boleh kosong")
+    } else if (phone == ""){
+        return alert("nomor telpon tidak boleh kosong")
+    } else if (subject == ""){
+        return alert("subject harus dipilih")
+    } else if (message == ""){
+        return alert("pesan tidak boleh kosong")
+    }
+
+    const emailReceiver = "yudhaprasty17@gmail.com"
+    let a = document.createElement("a")
+    a.href = 'mailto:${emailReceiver}?subject=${subject}&body= Halo, Saya ${name}, tolong hubungi nomor saya ${phone} untuk membahas sesuatu ${message}'
+    a.click()
+
+    let data = {
+        name,
+        email,
+        phone,
+        subject,
+        message
+    }
+
+    console.log(data)
+}
